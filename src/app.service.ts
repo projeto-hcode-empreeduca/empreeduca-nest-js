7e277e3fc9563b0bfa7015ed4c24327defcc1151
Anthony Rafael <anthonyribeiro1910@gmail.com>
2022-06-02 16:59:35 -0300

    deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `<h1>Seja bem-vindo ao projeto Nest JS!!!!</h1>`;
+    return `<h1>Seja bem-vindo ao projeto Nest JS na DigitalOcean!!!!</h1>`;
   }
 
   soma(a, b) {
